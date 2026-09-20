53f9ed5077ae971a0fae8c273624daec76363162
armeleger <a.kayisire@alustudent.com>
2026-09-20 21:00:18 +0000

    alu-web-development intranet assignment

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,19 +1,19 @@
-#!/usr/bin/python3
-"""BasicCache module"""
+#!/usr/bin/env python3
+"""Basic cache"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Caching system with no limit"""
+    """Basic Cache class"""
 
     def put(self, key, item):
         """Add an item in the cache"""
-        if key is not None and item is not None:
-            self.cache_data[key] = item
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
-
---
+++ html_advanced/README.md
@@ -0,0 +1,3 @@
+# HTML advanced project for web development.
+designing first webpage we need image for inspiration.
+![README](https://github.com/M-Pascal/alu-web-development/blob/master/html_advanced/photos/readme.png)
